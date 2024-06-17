a539eab6ce63fc4f82848c6fc82bff11d9ed9c4f
yennhi <yennhi81lqs@gmail.com>
2024-06-17 20:05:43 +0700

    update endpoint

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const endpoint = 'http://movtime-movies.eastasia.cloudapp.azure.com:8000';
-export const endpoint = 'http://localhost:8000';
+export const endpoint = 'http://movies-time.eastasia.cloudapp.azure.com:8000';
+// export const endpoint = 'http://localhost:8000';
